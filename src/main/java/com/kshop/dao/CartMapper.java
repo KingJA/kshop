3347fbb426b7bcd30046bf1a96759228c390832d
KingJA <kingjavip@gmail.com>
2017-06-20 23:10:44 +0800

    init mybatis-generator

---
+++ src/main/java/com/kshop/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.kshop.dao;
+
+import com.kshop.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
